db10bcbb17c4c53bc8f1c0c45127d4d7c5e4437b
ETesauro <emmanuel.tesauro@gmail.com>
2022-07-11 18:37:41 +0000

    Aggiunta la versione sequenziale (per testare la correttezza)

--- project/first/src/firstTry.cpp
+++ project/first/src/firstTry.cpp
@@ -2,6 +2,8 @@
 
 void init_matrix(int, int, float *);
 void print_matrix(int, int, float *, std::string);
+void sequential_matrix_multiplication(int, int, float *, float *, float *);
+int check_is_correct(int, int, float *, float *);
 
 int main() {
   // Settings
@@ -12,12 +14,17 @@ int main() {
   float *matrix_a = (float *)malloc(sizeof(float) * N * N);
   float *matrix_b = (float *)malloc(sizeof(float) * N * N);
   float *matrix_c = (float *)malloc(sizeof(float) * N * N);
+  float *matrix_sequential = (float *)malloc(sizeof(float) * N * N);
+
+  int res = 0;
 
   // Init
   init_matrix(N, N, matrix_a);
   init_matrix(N, N, matrix_b);
   memset(matrix_c, 0, sizeof(float) * N * N);
+  memset(matrix_sequential, 0, sizeof(float) * N * N);
 
+  // Start timer
   std::chrono::_V2::steady_clock::time_point start;
 
   // Device selector
@@ -64,19 +71,29 @@ int main() {
     std::cout << "Exception caught: " << e.what() << std::endl;
   }
 
+  // Get execution time
   std::chrono::_V2::steady_clock::time_point end = std::chrono::steady_clock::now();
   std::chrono::duration<double> elapsed_seconds = end - start;
   std::cout << "Time in seconds : " << elapsed_seconds.count() << std::endl;
+
+  // Sequential matrix multiplication (for test)
+  std::cout << "Doing sequential execution..." << std::endl;
+  sequential_matrix_multiplication(N, N, matrix_a, matrix_b, matrix_sequential);
+  res = check_is_correct(N, N, matrix_c, matrix_sequential);
 
   if (N < 10) {
     print_matrix(N, N, matrix_a, "MATRICE A");
     print_matrix(N, N, matrix_b, "MATRICE B");
     print_matrix(N, N, matrix_c, "MATRICE C");
+    print_matrix(N, N, matrix_sequential, "MATRICE SEQUENZIALE");
   }
+
+  std::cout << "RESULT: " << ((res == 1) ? "CORRECT" : "INCORRECT!") << std::endl;
 
   free(matrix_a);
   free(matrix_b);
   free(matrix_c);
+  free(matrix_sequential);
 }
 
 void init_matrix(int rows_size, int columns_size, float *matrix) {
@@ -93,7 +110,6 @@ void print_matrix(int rows_size, int columns_size, float *matrix, std::string ma
   std::cout << std::endl << matrix_name << std::endl;
   for (i = 0; i < rows_size * columns_size; i++) {
     if (rows_size < 10) {
-      printf("| ");
       std::cout << "| " << *(matrix + i) << " ";
     } else {
       std::cout << *(matrix + i) << " ";
@@ -105,4 +121,26 @@ void print_matrix(int rows_size, int columns_size, float *matrix, std::string ma
       if ((i + 1) % columns_size == 0 && i != 0) std::cout << std::endl;
     }
   }
+}
+
+void sequential_matrix_multiplication(int rows_size, int columns_size, float *matrix_a, float *matrix_b, float *matrix_res) {
+  int i, j, k;
+
+  for (i = 0; i < rows_size; i++) {
+    for (j = 0; j < columns_size; j++) {
+      for (k = 0; k < columns_size; k++) {
+        matrix_res[i * rows_size + j] += matrix_a[i * rows_size + k] * matrix_b[k * rows_size + j];
+      }
+    }
+  }
+}
+
+int check_is_correct(int rows_size, int columns_size, float *matrix_parallel, float *matrix_sequential) {
+  int i;
+  int res = 1;
+
+  for (int i = 0; i < rows_size * columns_size; i++)
+    if (matrix_parallel[i] != matrix_sequential[i]) res = 0;
+
+  return res;
 }
